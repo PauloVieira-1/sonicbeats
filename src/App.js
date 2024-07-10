c8fcb0d9ce94944852bb3f758dca80fa445fbb1f
Paulo Vieira <paulo1vieira.2004@gmail.com>
2024-07-10 17:16:59 +0200

    Navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import NavRounded from "../src/components/Navbar/Nav"
+import 'bootstrap/dist/css/bootstrap.css'
+import '../src/styles/colors.css'
+
+
+function App() {
+  return (
+    <NavRounded />
+  )
+}
+
+export default App;
---
+++ src/components/Navbar/Nav.js
@@ -0,0 +1,73 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Nav.css'
+import Logo from '../../assets/Logo.svg'
+import { useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+
+
+function NavRounded() {
+  
+  useEffect(() => {
+    let previousScroll = window.scrollY;
+
+    const scrollFeature = () => {
+        let currentScroll = window.scrollY;
+        const navContainer = document.getElementById("nav");
+
+        if (navContainer) { 
+            if (currentScroll > previousScroll) {
+                navContainer.style.top = "-100px";
+            } else {
+                navContainer.style.top = "10px";
+            }
+            previousScroll = currentScroll;
+        }
+    };
+
+    document.addEventListener("scroll", scrollFeature);
+
+}, []);
+  
+  return (
+    <Navbar id="nav" expand="lg" className="bg-transparent navbar-nav mx-auto nav-rounded">
+      <Container>
+        <Navbar.Brand href="#home">
+        <img
+              alt=""
+              src= {Logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+          Soniccraft</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Gallery</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav.Link className="mx-3" href="#home">
+            <Button className="btn-rounded" variant='outline-primary'>Request Product</Button>{' '}
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavRounded;
